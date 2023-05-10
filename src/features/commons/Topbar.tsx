07aba91d618bc8a0af72f86092fa02833d1e7d82
Mayank Mittal <msquare.pvt21@gmail.com>
2023-05-11 01:00:53 +0530

    [FIX] Color Fix

--- src/features/commons/Topbar.tsx
+++ src/features/commons/Topbar.tsx
@@ -4,7 +4,7 @@ import fuseLogo from "../../assets/fuselogo.svg";
 
 const Topbar = () => {
   return (
-    <nav className="w-full h-20 sticky top-0 bg-light-gray/60 backdrop-blur-xl flex justify-center py-7 z-50 md:bg-light-gray md:py-0">
+    <nav className="w-full h-20 sticky top-0 bg-light-gray/60 backdrop-blur-xl flex justify-center py-7 z-50 md:py-0 md:h-14">
       <div className="flex justify-between h-full items-center w-8/9 md:w-9/10">
         <span>
           <a href="/">
