9044c10b62707b43011af79d98b861af3b7e0f87
Nikolay <dnolnque@gmail.com>
2023-05-28 14:05:59 +0300

    fix

--- src/features/commons/Topbar.tsx
+++ src/features/commons/Topbar.tsx
@@ -4,7 +4,7 @@ import fuseLogo from "../../assets/fuselogo.svg";
 
 const Topbar = () => {
   return (
-    <nav className="w-full h-20 sticky top-0 bg-light-gray/60 backdrop-blur-xl flex justify-center py-7 z-40 md:py-0 md:h-14">
+    <nav className="w-full h-20 sticky top-0 bg-light-gray/60 backdrop-blur-xl flex justify-center py-7 z-40 md:py-0 md:h-[68px]">
       <div className="flex justify-between h-full items-center w-8/9 md:w-9/10 max-w-7xl" >
         <span>
           <a href="/">
